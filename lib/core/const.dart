0962312ef36887a82016d878274c431ca971b418
KerJen <kerjen01@gmail.com>
2023-05-29 15:06:05 +0300

    [FIX] Contract

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -4,7 +4,7 @@ const String testRpcUrl = 'http://127.0.0.1:7545';
 const String iphoneRpcUrl = 'http://192.168.1.146:7545';
 const String ethereumMainnet = 'https://mainnet.infura.io/v3/d590ba63a6434967844c8018caff0780';
 
-const String contractAddress = '0xAAbBFbF5E42657a51e28Cb225db31881a75626BF';
+const String contractAddress = '0xD36bDeF80FA4f64aAEaBc33078f91d2F717151fd';
 
 const List<String> shopCategories = [
   'Wordpress',
